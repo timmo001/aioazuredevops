b616d1f30cc84c8f2ff4f6146c79f8e2ce12dc71
Timmo <contact@timmo.xyz>
2020-05-27 11:16:19 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aioazuredevops",
-    version="1.3.3",
+    version="1.3.4",
     author="Timmo",
     author_email="contact@timmo.xyz",
     description="Get data from the Azure DevOps API.",
