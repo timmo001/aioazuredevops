6132c1ea98195cab7ffb6677f2c431fcedbc52ab
Aidan Timson <contact@timmo.xyz>
2022-02-20 00:43:16 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aioazuredevops",
-    version="1.4.1",
+    version="1.4.2",
     author="Timmo",
     author_email="contact@timmo.xyz",
     description="Get data from the Azure DevOps API.",
