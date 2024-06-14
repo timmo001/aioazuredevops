2998e0f93f64882e1dc3d14184646d4b8b9947f2
Aidan Timson <contact@timmo.xyz>
2024-06-15 00:23:37 +0100

    Add support for work item types (#25)

---
+++ aioazuredevops/models/work_item_types.py
@@ -0,0 +1,77 @@
+"""Work item types."""
+
+from dataclasses import dataclass
+from enum import StrEnum
+
+
+@dataclass
+class Field:
+    """Field."""
+
+    always_required: bool
+    reference_name: str
+    name: str
+    url: str
+    default_value: str | None = None
+    help_text: str | None = None
+
+
+class Category(StrEnum):
+    """Category."""
+
+    COMPLETED = "Completed"
+    IN_PROGRESS = "InProgress"
+    PROPOSED = "Proposed"
+    REMOVED = "Removed"
+    RESOLVED = "Resolved"
+
+
+@dataclass
+class State:
+    """State."""
+
+    name: str
+    color: str
+    category: Category
+
+
+@dataclass
+class Transition:
+    """Transition."""
+
+    to: str
+    actions: list[str] | None = None
+
+
+@dataclass
+class Icon:
+    """Icon."""
+
+    id: str
+    url: str
+
+
+@dataclass
+class WorkItemType:
+    """Work item type."""
+
+    name: str
+    reference_name: str
+    description: str
+    color: str
+    icon: Icon
+    is_disabled: bool
+    xml_form: str
+    fields: list[Field]
+    field_instances: list[Field]
+    transitions: dict[str, list[Transition]]
+    states: list[State]
+    url: str
+
+
+@dataclass
+class WorkItemTypesResult:
+    """Work item types result."""
+
+    count: int
+    value: list[WorkItemType]
